6f0c3afffa5e4cd55182aadec9c48d3581544e6a
Niklas Lindroos <niklas.lindroos@netlight.com>
2020-07-28 19:05:46 +0300

    Added eslint, airbnb preset, converted all react code to ts

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,11 +6,22 @@
  */
 
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
-function SEO({ description, lang, meta, title }) {
+interface SeoProps {
+  description?: string
+  lang?: string
+  meta?: HTMLMetaElement[]
+  title: string
+}
+
+const Seo: React.FunctionComponent<SeoProps> = ({
+  lang = `en`,
+  meta = [],
+  description = ``,
+  title,
+}) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -72,17 +83,4 @@ function SEO({ description, lang, meta, title }) {
   )
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
-
-export default SEO
+export default Seo
